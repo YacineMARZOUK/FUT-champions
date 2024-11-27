b33955b020f02b3b5eea7b80f50a36d0f732e224
YacineMARZOUK <yacinemarzou@gmail.com>
2024-11-27 15:41:19 +0100

    edit html

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+
+
 let createbtn = document.getElementById("createbtn");
 let importbtn = document.getElementById("importbtn");
-let creaeteform = document.getElementById("createform")
-let importform = document.getElementById("importform")
-let formulaire = document.getElementById("formulaire")
-let remplacant = document.getElementById("remplacant")
+let creaeteform = document.getElementById("createform");
+let importform = document.getElementById("importform");
+let formulaire = document.getElementById("formulaire");
+let remplacant = document.getElementById("remplacant");
 importbtn.addEventListener("click", () => {
 
     creaeteform.classList.add("hidden");
@@ -12,13 +14,7 @@ importbtn.addEventListener("click", () => {
     formulaire.classList.add("from-green-700", "via-green-800");
 });
 
-createbtn.addEventListener("click", () => {
 
-    importform.classList.add("hidden");
-    creaeteform.classList.remove("hidden");
-    formulaire.classList.add("from-red-700", "via-red-800");
-    formulaire.classList.remove("from-green-700", "via-green-800");
-});
 
 document.getElementById("createform").addEventListener("submit", function (event) {
     event.preventDefault(); // Empêche l'envoi du formulaire par défaut
@@ -59,9 +55,52 @@ document.getElementById("createform").addEventListener("submit", function (event
       alert(errors.join("\n"));
     } else {
       alert("Joueur créé avec succès !");
-      // Vous pouvez envoyer les données ici, par exemple avec fetch()
-      this.submit(); // Supprimez `event.preventDefault()` si vous voulez envoyer le formulaire.
+      
+      this.submit(); 
     }
   });
+  createbtn.addEventListener("click", () => {
+
+    importform.classList.add("hidden");
+    creaeteform.classList.remove("hidden");
+    formulaire.classList.add("from-red-700", "via-red-800");
+    formulaire.classList.remove("from-green-700", "via-green-800");
+    let remplacant = document.getElementById("replacant");
+  remplacant.innerHTML = `
+    <div class="bg-opacity-30 bg-[url('img/badge_gold.webp')] bg-center bg-no-repeat bg-cover w-full h-full flex flex-col justify-center items-center max-w-sm mx-auto h-auto shadow-none transition-shadow duration-300 cursor-pointer hover:shadow-lg hover:shadow-black">
+      <div class="flex justify-start">
+        <div class="text-xs text-[#393218] leading-2 my-6">
+          <p class="font-extrabold text-lg">90</p>
+          <p class="font-semibold text-lg">Cf</p>
+        </div>
+        <div class="text-center mt-5 text-[#393218] font-extra-bold mr-[10px] flex flex-col">
+          <img src="https://cdn.sofifa.net/players/158/023/25_120.png" alt="messi" class="w-[120px]">
+          <p class="font-extrabold text-[12px]">Messi</p>
+        </div>
+      </div>
+      <div class="boxes text-[12px] text-[#393218] grid grid-cols-6 grid-rows-2 gap-[4px]">
+        <div>PA</div>
+        <div>SH</div>
+        <div>PA</div>
+        <div>DR</div>
+        <div>DE</div>
+        <div>PH</div>
+        <div class="font-extrabold">80</div>
+        <div class="font-extrabold">87</div>
+        <div class="font-extrabold">90</div>
+        <div class="font-extrabold">94</div>
+        <div class="font-extrabold">33</div>
+        <div class="font-extrabold">64</div>
+      </div>
+      <div class="flages grid grid-cols-2 gap-4 items-center">
+        <img src="https://cdn.sofifa.net/flags/ar.png" alt="" class="w-[14px]">
+        <img src="https://cdn.sofifa.net/meta/team/239235/120.png" alt="" class="w-[14px]">
+      </div>
+    </div>
+  `;
+
+});
+
+ 
 
   
